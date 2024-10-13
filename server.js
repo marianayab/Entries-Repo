72ef8c728bdf538802440c7cea9e56e9d65a7484
marianayab <128246098+marianayab@users.noreply.github.com>
2024-10-14 04:19:56 +0500

    third commit

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
 
 const db = mysql.createConnection({
   user: process.env.USER,
